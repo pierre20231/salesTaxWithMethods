6b9484d12a1c884b1e05e4bcbbd5bf5c0abc0c61
James Warner <james.warner@indianhills.edu>
2023-12-07 13:45:49 -0600

    remove unused imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.text.DecimalFormat;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
        /*Setup any variables you need and call your methods
